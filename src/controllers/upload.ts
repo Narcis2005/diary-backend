9de4a3f92e615c28b66fa4df334f9c3e7824a64a
Chirilov Narcis <chirilov.narcis@yahoo.ro>
2022-06-27 20:17:16 +0300

    Fixed checks

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -1,19 +1,73 @@
 import { Request, Response } from "express";
 import fs from "fs";
+import multer, { FileFilterCallback } from 'multer';
+// https://stackoverflow.com/questions/45154069/display-uploaded-image-with-multer
+
+const storage = multer.diskStorage({
+    destination: "static/images",
+    filename: function (req, file, cb) {
+        let filename = Date.now().toString();
+        switch (file.mimetype) {
+            case "image/png":
+                filename = filename + ".png";
+                break;
+            case "image/jpeg":
+                filename = filename + ".jpeg";
+                break;
+            case "image/jpg":
+            filename = filename + ".jpgg";
+            break;
+            default:
+                break;
+        }
+        cb(null, filename);
+    },
+   
+});
+const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback): void => {
+    let isValid = false;
+    switch (file.mimetype) {
+      case "image/png":
+          isValid = true;
+          break;
+      case "image/jpeg":
+          isValid = true;
+          break;
+      case "image/jpg":
+          isValid = true;
+      break;
+      default:
+          break;
+  }
+    const error = isValid ? null : new Error('Invalid mime type!');
+    if (error) {
+      return cb(error);
+    }
+        cb( null, isValid);
+  };
+const upload = multer({ storage,  fileFilter, limits:{fileSize: 5242880}}).single("file");
+
 export const Upload = (req: Request, res: Response) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    //add resize and compression
-    if (req.file) {
-        if (!req.file.mimetype.startsWith("image/") || req.file.size >= 5242880) {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
             res.status(400).send({ message: "Your file must be an image with the size lower than 5mb" });
             return;
+
+        } else if (err) {
+            res.status(500).send({message: "Your file must be an image with the size lower than 5mb. If this is the case, this might be a server issue"});
+            return;
         }
-        const formData = req.file;
-        if (req.user.imagePath !== "static/images/profilePlaceholder.png") {
-            fs.unlinkSync(req.user.imagePath);
+        if (req.file) {
+
+            const formData = req.file;
+            if (req.user.imagePath !== "static/images/profilePlaceholder.png") {
+                fs.unlinkSync(req.user.imagePath);
+            }
+            res.send(formData.filename);
+            return;
         }
-        res.send(formData.filename);
-        return;
-    }
-    res.sendStatus(400);
+        res.sendStatus(400);
+    });
+
 };
--- src/routes/uploads/index.ts
+++ src/routes/uploads/index.ts
@@ -3,26 +3,6 @@ import express from "express";
 import { Upload } from "../../controllers/upload";
 import privateEndpoint from "../../middleware/privateEndpoint";
 const router = express.Router();
-import multer from "multer";
 
-// https://stackoverflow.com/questions/45154069/display-uploaded-image-with-multer
-const storage = multer.diskStorage({
-    destination: "static/images",
-    filename: function (req, file, cb) {
-        let filename = Date.now().toString();
-        switch (file.mimetype) {
-            case "image/png":
-                filename = filename + ".png";
-                break;
-            case "image/jpeg":
-                filename = filename + ".jpeg";
-                break;
-            default:
-                break;
-        }
-        cb(null, filename);
-    },
-});
-const upload = multer({ storage });
-router.post("/", [privateEndpoint, upload.single("file")], Upload);
+router.post("/", privateEndpoint, Upload);
 export default router;
